65a1242e84e9b88d60d111dde23a9eb6723706ed
NickHardeman <nickhardeman@gmail.com>
2018-11-08 14:06:49 -0500

    update to auto load shaders, images, etc.

--- src/ofxFileWatcher.cpp
+++ src/ofxFileWatcher.cpp
@@ -12,71 +12,145 @@
 ofxFileWatcher::ofxFileWatcher() {
     bHasListeners = false;
     setGarbageCollection( true );
-    nextCheckTimeMillis = 10;
+    nextCheckTimeMillis = 1000;
     setInterval( 1000 );
-    start();
 }
 
 //--------------------------------------------------------------
 bool ofxFileWatcher::shouldRemoveFile( const shared_ptr<WatchFile>& af ) {
-    return af->bDeleted;
+    return (!af || af->bDeleted);
 }
 
 //--------------------------------------------------------------
-void ofxFileWatcher::addFile( string aPath ) {
-    ofFile tfile( aPath );
-    addFile( aPath, tfile.getBaseName() );
+void ofxFileWatcher::add( string aPath ) {
+    add( aPath, ofFilePath::getBaseName(aPath) );
 }
 
 //--------------------------------------------------------------
-void ofxFileWatcher::addFile( string aPath, string aCuteLilNickName ) {
-    add( aPath, aCuteLilNickName, NULL, NULL );
+void ofxFileWatcher::add( string aPath, string aCuteLilNickName ) {
+    if( ofFile::doesFileExist(aPath) ) {
+        auto wf = make_shared<WatchFile>();
+        wf->set(aCuteLilNickName, ofToDataPath( aPath, true), false );
+        files.push_back( wf );
+    } else {
+        ofLogWarning("ofxFileWatch :: add file: file does not exist: ") << aPath;
+    }
 }
 
 //--------------------------------------------------------------
-void ofxFileWatcher::addImage( string aPath, ofImage* aImg ) {
-    ofFile tfile( aPath );
-    addImage( aPath, aImg, tfile.getBaseName() );
+void ofxFileWatcher::add( string aPath, ofImage* aImg, bool abAutoLoad ) {
+    add( aPath, aImg, ofFilePath::getBaseName(aPath), abAutoLoad );
 }
 
 //--------------------------------------------------------------
-void ofxFileWatcher::addImage( string aPath, ofImage* aImg, string aCuteLilNickName ) {
-    add( aPath, aCuteLilNickName, aImg, NULL );
+void ofxFileWatcher::add( string aPath, ofImage* aImg, string aCuteLilNickName, bool abAutoLoad ) {
+    if(ofFile::doesFileExist(aPath)) {
+        auto wf = make_shared<WatchImage>();
+        wf->set(aCuteLilNickName, ofToDataPath( aPath, true), abAutoLoad );
+        wf->img = aImg;
+        if( wf->img != NULL && !wf->img->isAllocated() ) {
+            wf->load();
+        }
+        files.push_back( wf );
+    } else {
+        ofLogWarning("ofxFileWatch :: add image: file does not exist: ") << aPath;
+    }
 }
 
 //--------------------------------------------------------------
-void ofxFileWatcher::addTexture( string aPath, ofTexture* aTex ) {
-    ofFile tfile( aPath );
-    addTexture( aPath, aTex, tfile.getBaseName() );
+void ofxFileWatcher::add( string aPath, ofTexture* aTex, bool abAutoLoad, bool abTex2d ) {
+    add( aPath, aTex, ofFilePath::getBaseName(aPath), abAutoLoad, abTex2d );
 }
 
 //--------------------------------------------------------------
-void ofxFileWatcher::addTexture( string aPath, ofTexture* aTex, string aCuteLilNickName ) {
-    add( aPath, aCuteLilNickName, NULL, aTex );
+void ofxFileWatcher::add( string aPath, ofTexture* aTex, string aCuteLilNickName, bool abAutoLoad, bool abTex2d ) {
+    if(ofFile::doesFileExist(aPath)) {
+        auto wf = make_shared<WatchTexture>();
+        wf->set(aCuteLilNickName, ofToDataPath( aPath, true), abAutoLoad );
+        wf->bTex2d = abTex2d;
+        wf->tex = aTex;
+        if( wf->tex != NULL && !wf->tex->isAllocated() ) {
+            wf->load();
+        }
+        files.push_back( wf );
+    } else {
+        ofLogWarning("ofxFileWatch :: add texture: file does not exist: ") << aPath;
+    }
 }
 
 //--------------------------------------------------------------
-void ofxFileWatcher::add( string aPath, string aCuteLilNickName, ofImage* aImg, ofTexture* aTex ) {
-    if( !ofFile::doesFileExist( aPath )) {
-        ofLogWarning("ofxFileWatcher file does not exist!! - ") << aPath;
-        return;
+void ofxFileWatcher::add( string aVertPath, string aFragPath, string aGeomPath, ofShader* aShader, bool abAutoLoad ) {
+    string tnickName = "temp";
+    if( aVertPath != "" ) {
+        tnickName = ofFilePath::getBaseName(aVertPath);
+    } else {
+        tnickName = ofFilePath::getBaseName(aFragPath);
+    }
+    add( aVertPath, aFragPath, aGeomPath, aShader, tnickName, abAutoLoad );
+}
+
+//--------------------------------------------------------------
+void ofxFileWatcher::add( string aVertPath, string aFragPath, string aGeomPath, ofShader* aShader, string aCuteLilNickName, bool abAutoLoad ) {
+    vector< shared_ptr<WatchShader> > newWatchers;
+    if( aVertPath != "" ) {
+        if( ofFile::doesFileExist(aVertPath) ) {
+            auto wf = make_shared<WatchShader>();
+            wf->set(aCuteLilNickName, ofToDataPath( aVertPath, true), abAutoLoad);
+            newWatchers.push_back( wf );
+        }
+    }
+    if( aFragPath != "" ) {
+        if( ofFile::doesFileExist(aFragPath) ) {
+            auto wf = make_shared<WatchShader>();
+            wf->set(aCuteLilNickName, ofToDataPath( aFragPath, true), abAutoLoad);
+            newWatchers.push_back( wf );
+        }
+    }
+    if( aGeomPath != "" ) {
+        if( ofFile::doesFileExist(aGeomPath) ) {
+            auto wf = make_shared<WatchShader>();
+            wf->set(aCuteLilNickName, ofToDataPath( aGeomPath, true), abAutoLoad);
+            newWatchers.push_back( wf );
+        }
     }
     
-    shared_ptr< WatchFile > wf( new WatchFile() );
-    wf->filePath    = ofToDataPath(aPath, true);
-    wf->name        = aCuteLilNickName;
-    wf->img         = aImg;
-    wf->tex         = aTex;
-    
-    if( wf->img != NULL && !wf->img->isAllocated() ) {
-        ofLoadImage( *wf->img, wf->filePath );
+    if( newWatchers.size() ) {
+        for( int i = 0; i < newWatchers.size(); i++ ) {
+            newWatchers[i]->setShaderPaths( aVertPath, aFragPath, aGeomPath );
+            newWatchers[i]->shader = aShader;
+            if( newWatchers[i]->shader != NULL && !newWatchers[i]->shader->isLoaded() ) {
+                newWatchers[i]->load();
+            }
+        }
+        files.insert( files.end(), newWatchers.begin(), newWatchers.end() );
+    } else {
+        ofLogWarning("ofxFileWatcher :: addShader : no valid shader paths! XXXX ");
     }
-    if( wf->tex != NULL && !wf->tex->isAllocated() ) {
-        ofLoadImage( *wf->tex, wf->filePath );
-    }
-    
-    files.push_back( wf );
 }
+
+////--------------------------------------------------------------
+//void ofxFileWatcher::add( string aPath, string aCuteLilNickName, ofImage* aImg, ofTexture* aTex, bool abAutoLoad ) {
+//    if( !ofFile::doesFileExist( aPath )) {
+//        ofLogWarning("ofxFileWatcher file does not exist!! - ") << aPath;
+//        return;
+//    }
+//    
+//    auto wf = make_shared<WatchFile>();
+//    wf->filePath    = ofToDataPath(aPath, true);
+//    wf->name        = aCuteLilNickName;
+//    wf->img         = aImg;
+//    wf->tex         = aTex;
+//    wf->bAutoLoad   = abAutoLoad;
+//    
+//    if( wf->bAutoLoad && wf->img != NULL && !wf->img->isAllocated() ) {
+//        ofLoadImage( *wf->img, wf->filePath );
+//    }
+//    if( wf->bAutoLoad && wf->tex != NULL && !wf->tex->isAllocated() ) {
+//        ofLoadImage( *wf->tex, wf->filePath );
+//    }
+//    
+//    files.push_back( wf );
+//}
 
 //--------------------------------------------------------------
 void ofxFileWatcher::start() {
@@ -117,16 +191,20 @@ void ofxFileWatcher::setGarbageCollection( bool aB ) {
 //--------------------------------------------------------------
 void ofxFileWatcher::update( ofEventArgs& args ) {
     int emills = ofGetElapsedTimeMillis();
-    if( emills >= nextCheckTimeMillis ) {
+    if( emills >= nextCheckTimeMillis && ofGetFrameNum() > 60 ) {
         // check the files here //
         for( int i = 0; i < files.size(); i++ ) {
-            shared_ptr< WatchFile >& wf = files[i];
-            if( !ofFile::doesFileExist(wf->filePath) ) {
+            if( i >= (int)files.size()) break;
+            shared_ptr< WatchFile > wf = files[i];
+            if( !wf ) continue;
+            if( wf->filePath == "" || !ofFile::doesFileExist(wf->filePath) ) {
                 if( !wf->bDeleted ) {
                     Event tevent;
                     tevent.filePath = wf->filePath;
                     tevent.name     = wf->name;
+                    tevent.file     = wf->file;
                     tevent.type     = EVENT_DEL;
+                    cout << "WatchDog :: passing a delete file event " << endl;
                     ofNotifyEvent( FileModEvent, tevent, this);
                 }
                 wf->bDeleted = true;
@@ -140,14 +218,20 @@ void ofxFileWatcher::update( ofEventArgs& args ) {
 //                cout << "wf->name: " << wf->name << " time: " << attrib.st_mtime << endl;
                 if( wf->modTime != attrib.st_mtime ) {
                     
-                    // check to see if we need to load a tex or image //
-                    if( wf->img != NULL ) {
-                        ofLoadImage( *wf->img, wf->filePath );
+                    if( wf->bAutoLoad ) {
+                        bool bOk = wf->load();
+//                        ofLogNotice("ofxFileWatcher :: load: " ) << bOk << " file: " << wf->file;
+                        cout << ("ofxFileWatcher :: load: " ) << bOk << " file: " << wf->file << " | " << ofGetFrameNum() << endl;
                     }
                     
-                    if( wf->tex != NULL ) {
-                        ofLoadImage( *wf->tex, wf->filePath );
-                    }
+                    // check to see if we need to load a tex or image //
+//                    if( wf->bAutoLoad && wf->img != NULL ) {
+//                        ofLoadImage( *wf->img, wf->filePath );
+//                    }
+//                    
+//                    if( wf->bAutoLoad && wf->tex != NULL ) {
+//                        ofLoadImage( *wf->tex, wf->filePath );
+//                    }
                     
                     wf->modTime = attrib.st_mtime;
                     
@@ -155,7 +239,9 @@ void ofxFileWatcher::update( ofEventArgs& args ) {
                     Event tevent;
                     tevent.filePath = wf->filePath;
                     tevent.name     = wf->name;
+                    tevent.file     = wf->file;
                     tevent.type     = EVENT_MOD;
+//                    cout << "WatchDog :: passing a mod file event " << endl;
                     ofNotifyEvent( FileModEvent, tevent, this);
                 }
             }
--- src/ofxFileWatcher.h
+++ src/ofxFileWatcher.h
@@ -18,41 +18,97 @@ public:
     
     class WatchFile {
     public:
-        WatchFile() {
-            modTime = 0;
-            bDeleted = false;
-            tex = NULL;
-            img = NULL;
+        void set(string aname, string afilepath, bool abAutoLoad) {
+            name = aname;
+            filePath = afilepath;
+            file = ofFilePath::getBaseName(filePath)+"."+ofFilePath::getFileExt(filePath);
+            bAutoLoad = abAutoLoad;
         }
         
-        ofImage* img;
-        ofTexture* tex;
+        virtual bool load() { return false; }
+        string file = "";
         
-        string filePath;
-        string name;
-        long modTime;
-        bool bDeleted;
+        string filePath = "";
+        string name = "";
+        bool bAutoLoad=false;
+        long modTime = 0;
+        bool bDeleted=false;
+        
     };
+    
+    class WatchImage : public WatchFile {
+    public:
+        
+        bool load() {
+            if( bAutoLoad && img != NULL ) {
+                return ofLoadImage( *img, filePath );
+            }
+            return false;
+        }
+        ofImage* img = NULL;
+    };
+    
+    class WatchTexture : public WatchFile {
+    public:
+        bool load() {
+            if( bAutoLoad && tex != NULL ) {
+                if( bTex2d ) ofDisableArbTex();
+                bool bok = ofLoadImage( *tex, filePath );
+                if( bTex2d ) ofEnableArbTex();
+                return bok;
+            }
+            return false;
+        }
+        ofTexture* tex = NULL;
+        bool bTex2d = false;
+    };
+    
+    class WatchShader : public WatchFile {
+    public:
+        bool load() {
+            if( bAutoLoad && shader != NULL ) {
+                return shader->load( vertPath, fragPath, geomPath );
+            }
+            return false;
+        }
+        
+        void setShaderPaths( string aVertPath, string aFragPath, string aGeomPath ) {
+            vertPath = aVertPath;
+            fragPath = aFragPath;
+            geomPath = aGeomPath;
+        }
+        
+        ofShader* shader = NULL;
+        
+        string vertPath = "";
+        string fragPath = "";
+        string geomPath = "";
+    };
+    
     
     class Event {
     public:
         int type;
         string filePath;
         string name;
+        string file;
     };
     
     ofxFileWatcher();
     
     static bool shouldRemoveFile( const shared_ptr<WatchFile>& af );
     
-    void addFile( string aPath );
-    void addFile( string aPath, string aCuteLilNickName );
+    void add( string aPath );
+    void add( string aPath, string aCuteLilNickName );
     
-    void addImage( string aPath, ofImage* aImg );
-    void addImage( string aPath, ofImage* aImg, string aCuteLilNickName );
+    void add( string aPath, ofImage* aImg, bool abAutoLoad=true );
+    void add( string aPath, ofImage* aImg, string aCuteLilNickName, bool abAutoLoad=true );
     
-    void addTexture( string aPath, ofTexture* aTex );
-    void addTexture( string aPath, ofTexture* aTex, string aCuteLilNickName );
+    void add( string aPath, ofTexture* aTex, bool abAutoLoad=true, bool abTex2d=false );
+    void add( string aPath, ofTexture* aTex, string aCuteLilNickName, bool abAutoLoad=true, bool abTex2d=false );
+    
+    void add( string aVertPath, string aFragPath, string aGeomPath, ofShader* aShader, bool abAutoLoad=true );
+    void add( string aVertPath, string aFragPath, string aGeomPath, ofShader* aShader, string aCuteLilNickName, bool abAutoLoad=true );
     
     void start();
     void stop();
@@ -66,7 +122,7 @@ public:
     ofEvent< Event > FileModEvent;
     
 protected:
-    void add( string aPath, string aCuteLilNickName, ofImage* aImg, ofTexture* aTex );
+//    void add( string aPath, string aCuteLilNickName, ofImage* aImg, ofTexture* aTex, bool abAutoLoad );
     void update( ofEventArgs& args );
     
     bool bHasListeners, bAutoRemoveDelFiles;
